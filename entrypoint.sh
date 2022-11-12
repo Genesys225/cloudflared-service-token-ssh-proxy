ecb53566c58c0946809b871fb8915888bef77a4c
Genesys225 <gene.korn@gmail.com>
2022-11-13 00:31:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 export HOST=$1
-export COMMAND=$2
-export CLIENT_ID=$3
-export CLIENT_SECRET=$4
+export USER=$2
+export COMMAND=$3
+export CLIENT_ID=$4
+export CLIENT_SECRET=$5
 
 envsubst < "/root/ssh-client.conf" > "/root/.ssh/config" 
 envsubst < "/root/expect.sh" > "/root/expect-final.sh" 
-PUBLIC_KEY="$(echo $5 | base64 --decode)"
-PRIVATE_KEY="$(echo $6 | base64 --decode)"
+PUBLIC_KEY="$(echo $6 | base64 --decode)"
+PRIVATE_KEY="$(echo $7 | base64 --decode)"
 sh -c "cat > /root/.ssh/id_rsa << 'EOL'
 $PRIVATE_KEY
 EOL"
@@ -18,4 +19,4 @@ EOL"
 
 chmod 0600 /root/.ssh/id_rsa
 cat /root/.ssh/config
-/root/expect-final.sh | tee $GITHUB_OUTPUT
+output=$(/root/expect-final.sh | tee $GITHUB_OUTPUT)
