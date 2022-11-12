6d16a3d55cccf5fb05a2dfa829589c07c055b99a
Genesys225 <gene.korn@gmail.com>
2022-11-13 00:30:19 +0200

    Update expect.sh

--- expect.sh
+++ expect.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 
-spawn ssh -v -o "StrictHostKeyChecking no" -p 22 bestme-web@b-ssh-stage.bestme.net/ssh
+spawn ssh -v -o "StrictHostKeyChecking no" -p 22 $USER@$HOST
 expect "$ "
 send "${COMMAND}\r"
 expect "$ "
