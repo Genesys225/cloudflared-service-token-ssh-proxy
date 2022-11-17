0e32af78d6bbf977198fc562a4eb316eb1ae07c5
Genesys225 <gene.korn@gmail.com>
2022-11-18 00:05:50 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ export CLIENT_ID=$4
 export CLIENT_SECRET=$5
 
 envsubst < "/root/ssh-client.conf" > "/root/.ssh/config" 
-# envsubst < "/root/expect.sh" > "/root/expect-final.sh" 
+envsubst < "/root/expect.sh" > "/root/expect-final.sh" 
 PUBLIC_KEY="$(echo $6 | base64 --decode)"
 PRIVATE_KEY="$(echo $7 | base64 --decode)"
 sh -c "cat > /root/.ssh/id_rsa << 'EOL'
