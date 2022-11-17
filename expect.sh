0e6b8e2112d1ae4d9470a6e9b84989e2336c45fd
Genesys225 <gene.korn@gmail.com>
2022-11-17 21:43:37 +0200

    Update expect.sh

--- expect.sh
+++ expect.sh
@@ -2,6 +2,10 @@
 
 spawn ssh -v -o "StrictHostKeyChecking no" -p 22 $USER@$HOST
 expect "$ "
-send "${COMMAND}\r"
-expect "$ "
+while IFS= read -r line; do
+    send "$line\r"
+    expect "$ "
+done <<< "${COMMAND}"
+# send "${COMMAND}\r"
+# expect "$ "
 send "exit\r"
